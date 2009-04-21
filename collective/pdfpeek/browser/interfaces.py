0e50ee57d31ff63056e56d5e6a637e3b16c05400
dbfrombrc <devnull@localhost>
2009-04-21 02:09:23 +0000

    refactored browser.interfaces

--- collective/pdfpeek/browser/interfaces.py
+++ collective/pdfpeek/browser/interfaces.py
@@ -1,8 +1,22 @@
+##########################################################################
+#                                                                        #
+#        copyright (c) 2009 David Brenneman                              #
+#        open-source under the GPL v2.1 (see LICENSE.txt)                #
+#                                                                        #
+##########################################################################
+
+"""
+PdfPeek browser view interfaces
+"""
+
+__author__ = """David Brenneman <db@davidbrenneman.com>"""
+__docformat__ = 'plaintext'
+
 from plone.theme.interfaces import IDefaultPloneLayer
 
-class IThemeSpecific(IDefaultPloneLayer):
+class IPdfPeekThemeSpecific(IDefaultPloneLayer):
     """Marker interface that defines a Zope 3 skin layer.
        If you need to register a viewlet only for the
-       "PdfPeek Theme" skin, this interface must be its layer
-       (in theme/viewlets/configure.zcml).
+       "PdfPeek" skin, this interface must be its layer
+       (in viewlets/configure.zcml).
     """
