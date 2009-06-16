d85171db8dc282e37cbb7740092f484f11bafe74
dbfrombrc <devnull@localhost>
2009-06-16 17:38:32 +0000

    removed skin initialization boilerplate

--- collective/pdfpeek/__init__.py
+++ collective/pdfpeek/__init__.py
@@ -1,7 +1,3 @@
-# Register our skins directory - this makes it available via portal_skins.
-from Products.CMFCore.DirectoryView import registerDirectory
-GLOBALS = globals()
-registerDirectory('skins', GLOBALS)
 
 def initialize(context):
     """Intializer called when used as a Zope 2 product."""
